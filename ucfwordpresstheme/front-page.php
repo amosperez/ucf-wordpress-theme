4fce33e992347bb6517b9061f90b8f2e14b08bcf
amosperez <amos7perez@gmail.com>
2023-10-31 03:06:03 -0400

    home page

--- ucfwordpresstheme/front-page.php
+++ ucfwordpresstheme/front-page.php
@@ -2,10 +2,80 @@
 
     <div class="container">
 
-        <h1><?php the_title();?></h1>
+        <!-- <h1><?php the_title();?></h1> -->
 
-        <?php get_template_part('includes/section', 'content'); ?>
-
+        <!-- <?php get_template_part('includes/section', 'content'); ?> -->
     </div>
+
+
+    <div class="media-background-container">
+
+        <video class="media-background object-fit-cover" muted playsinline loop autoplay>
+            <source src="/wp-content/themes/ucfwordpresstheme/images/header-video-3.mp4">
+        </video>
+    <!-- Your content here... -->
+        <div class="pb-15 pt-20">
+            <div class="container">
+            <div class="pr-5">            
+            <p class="display-4 font-weight-black pr-5 text-primary">We empower students to <span class="text-white">connect</span>, <span class="text-white">involve</span>  themselves, and create a positive <span class="text-white">impact</span> on campus life.</p>
+
+            <!-- <p class="display-4 font-weight-black pr-5 text-primary">We empower students to <span class="">connect, engage, and create a positive impact</span> on campus life.</p> -->
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-inverse py-10 bg-design-pattern">
+    <img src="/wp-content/themes/ucfwordpresstheme/images/bg-design-pattern.svg" alt="..." class="design-pattern">
+        <div class="container">
+            <h2 class="mb-5 display-4 text-primary">Mission</h2>
+
+            <div class="row">
+                <p class="col-12 col-sm-9 col-md-8">The mission of the Office of Student Involvement (OSI) is to provide quality programs, services, and leadership oppurtunities that enrich students’ academic endeavors and enhance the campus environment. Students and staff collaborate to develop programs and services which encourage student’s personal growth, to promote civic responsibility, to embrace differences, and to connect students to the campus and the community.</p>
+            </div>
+        </div>
+
+        
+    </div>
+
+
+
+
+
+
+    <div class="container mt-10">
+    <div class="row">
+        <div class="col">            
+            <!-- <p class="display-4 font-weight-black pr-5">We empower students to <span class="heavy-underline">connect, engage, and create a positive impact</span> on campus life.</p> -->
+            <p class="h1 font-weight-black pr-5">We empower students to <span class="heavy-underline">connect</span>, <span class="heavy-underline">involve</span>  themselves, and create a positive <span class="heavy-underline">impact</span> on campus life.</p>
+
+        </div>
+        <div class="col">            
+            <!-- <p class="display-4 font-weight-black pr-5">We empower students to <span class="heavy-underline">connect, engage, and create a positive impact</span> on campus life.</p> -->
+            <p class="display-4 font-weight-black pr-5">We empower students to <span class="heavy-underline">connect</span>, <span class="heavy-underline">involve</span>  themselves, and create a positive <span class="heavy-underline">impact</span> on campus life.</p>
+
+        </div>
+    </div>
+    </div>
+
+    <div class="mt-10 bg-primary py-10">
+        <div class="container">
+        <div class="pr-5">            
+        <p class="display-4 font-weight-black pr-5">We empower students to <span class="heavy-underline">connect</span>, <span class="heavy-underline">involve</span>  themselves, and create a positive <span class="heavy-underline">impact</span> on campus life.</p>
+        </div>
+        </div>
+    </div>
+
+    <div class="bg-inverse py-10">
+        <div class="container">
+        <div class="pr-5">            
+        <p class="display-4 font-weight-black pr-5 text-primary">We empower students to <span class="text-white">connect</span>, <span class="text-white">involve</span>  themselves, and create a positive <span class="text-white">impact</span> on campus life.</p>
+
+        <!-- <p class="display-4 font-weight-black pr-5 text-primary">We empower students to <span class="">connect, engage, and create a positive impact</span> on campus life.</p> -->
+        </div>
+        </div>
+    </div>
+
+    
 <?php get_footer(); ?>
 
--- ucfwordpresstheme/template-about.php
+++ ucfwordpresstheme/template-about.php
@@ -8,136 +8,175 @@ Template Name: About
 <?php get_header(); ?>
 
     <!-- I need a header up here -->
-    <h1><?php the_title();?></h1>
+    
     <!-- I need a header up here -->
 
+    <div class="py-15 header-container"> 
+        <img src="/wp-content/themes/ucfwordpresstheme/images/bg-banner-placeholder.svg" class="w-100 header-background" alt="Responsive image">        
+        <div class="container">         
+            <h1 class="display-1 text-primary font-slab-serif font-weight-bold"><?php the_title();?></h1>
+        </div>
+    </div>
+
     <div class="container mt-10">
-        <div class="pr-5">            
-            <p class="display-4 font-weight-black pr-5">We empower students to <span class="heavy-underline">connect, engage, and create a positive impact</span> on campus life.</p>
+        <h2 class="mb-5 display-4">Mission</h2>
+
+        <div class="row">
+            <p class="col-12 col-sm-9 col-md-8">The mission of the Office of Student Involvement (OSI) is to provide quality programs, services, and leadership oppurtunities that enrich students’ academic endeavors and enhance the campus environment. Students and staff collaborate to develop programs and services which encourage student’s personal growth, to promote civic responsibility, to embrace differences, and to connect students to the campus and the community.</p>
+        </div>
+
+    </div>
+    
+    <div class="bg-inverse py-10 bg-design-pattern mt-10">
+    <img src="/wp-content/themes/ucfwordpresstheme/images/bg-design-pattern.svg" alt="..." class="design-pattern">
+        <div class="container">
+            <h2 class="mb-5 display-4 text-primary">Mission</h2>
+
+            <div class="row">
+                <p class="col-12 col-sm-9 col-md-8">The mission of the Office of Student Involvement (OSI) is to provide quality programs, services, and leadership oppurtunities that enrich students’ academic endeavors and enhance the campus environment. Students and staff collaborate to develop programs and services which encourage student’s personal growth, to promote civic responsibility, to embrace differences, and to connect students to the campus and the community.</p>
+            </div>
+        </div>
+
+        
+    </div>
+
+    <div class="container mt-20">
+        <h2 class="mb-5 display-4">Priorities</h2>
+
+        <div >
+            <ol class="row mb-0">
+            <li class="col-12 col-sm-9 col-md-8 mb-3">Create intentional opportunities that help students engage in activities that encourage personal growth, promote civic responsibility, and connect students to the campus and the community.</li>
+            <li class="col-12 col-sm-9 col-md-8 mb-3">Promote environments and programs that support diversity, inclusion and equity</li>
+            <li class="col-12 col-sm-9 col-md-8 mb-3">Enrich students sense of belonging to UCF through positive expression of school spirit and pride</li>
+
+            <li class="col-12 col-sm-9 col-md-8 mb-3">Provide programs and services that encourage and enhance student learning in the following areas:</li>
+            <ul>
+            <li>Career Readiness</li>
+            <li>Personal Growth</li>
+            <li>Responsible Advocacy and Civic Responsibility</li>
+            <li>Diversity, Inclusion and Equity</li>
+            </ul>
+            </ol>
+            <!-- FIX ME -->
+            <!-- <p class="col-12 col-sm-9 col-md-8">Create intentional opportunities that help students engage in activities that encourage personal growth, promote civic responsibility, and connect students to the campus and the community.</p>
+            <p class="col-12 col-sm-9 col-md-8">Promote environments and programs that support diversity, inclusion and equity</p>
+            <p class="col-12 col-sm-9 col-md-8">Enrich students sense of belonging to UCF through positive expression of school spirit and pride</p>
+
+            <p class="col-12 col-sm-9 col-md-8">Provide programs and services that encourage and enhance student learning in the following areas:</p>
+            <ul>
+            <li>Career Readiness</li>
+            <li>Personal Growth</li>
+            <li>Responsible Advocacy and Civic Responsibility</li>
+            <li>Diversity, Inclusion and Equity</li>
+            </ul> -->
         </div>
     </div>
 
     <div class="container mt-20">
-        <ul class="list-unstyled row ucf-post-list-items flex-wrap">
-            <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
-                <a class="">
-                    <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail.jpg" alt="..." class="w-75 w-md-100">
-                    <p class="mb-1 mt-3 h4">Employee Name</p>
-                    <p class="mb-1">Title</p>
-                    <p class="mb-5">Contact Info</p>
-                </a>
-            </li>
-            <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
-                <a class="">
-                    <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail.jpg" alt="..." class="w-75 w-md-100">
-                    <p class="mb-1 mt-3 h4">Employee Name</p>
-                    <p class="mb-1">Title</p>
-                    <p class="mb-5">Contact Info</p>
-                </a>
-            </li>
-            <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
-                <a class="">
-                    <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail.jpg" alt="..." class="w-75 w-md-100">
-                    <p class="mb-1 mt-3 h4">Employee Name</p>
-                    <p class="mb-1">Title</p>
-                    <p class="mb-5">Contact Info</p>
-                </a>
-            </li>
-            <!-- <div class="w-75 w-md-100"></div> FIX ME: Perhaps, this is unecessary. -->
-            <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
-                <a class="">
-                    <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail.jpg" alt="..." class="w-75 w-md-100">
-                    <p class="mb-1 mt-3 h4">Employee Name</p>
-                    <p class="mb-1">Title</p>
-                    <p class="mb-5">Contact Info</p>
-                </a>
-            </li>
-            <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
-                <a class="">
-                    <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail.jpg" alt="..." class="w-75 w-md-100">
-                    <p class="mb-1 mt-3 h4">Employee Name</p>
-                    <p class="mb-1">Title</p>
-                    <p class="mb-5">Contact Info</p>
-                </a>
-            </li>
-            <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
-                <a class="">
-                    <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail.jpg" alt="..." class="w-75 w-md-100">
-                    <p class="mb-1 mt-3 h4">Employee Name</p>
-                    <p class="mb-1">Title</p>
-                    <p class="mb-5">Contact Info</p>
-                </a>
-            </li>
-            <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
-                <a class="">
-                    <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail.jpg" alt="..." class="w-75 w-md-100">
-                    <p class="mb-1 mt-3 h4">Employee Name</p>
-                    <p class="mb-1">Title</p>
-                    <p class="mb-5">Contact Info</p>
-                </a>
-            </li>
-            <!-- <div class="w-75 w-md-100"></div> FIX ME: Perhaps, this is unecessary. -->
-        </ul>
-        <ul class="mt-10 list-unstyled row ucf-post-list-items flex-wrap">
-            <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
-                <a class="">
-                    <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail.jpg" alt="..." class="w-75 w-md-100">
-                    <p class="mb-1 mt-3 h4">Employee Name</p>
-                    <p class="mb-1">Title</p>
-                    <p class="mb-5">Contact Info</p>
-                </a>
-            </li>
-            <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
-                <a class="">
-                    <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail.jpg" alt="..." class="w-75 w-md-100">
-                    <p class="mb-1 mt-3 h4">Employee Name</p>
-                    <p class="mb-1">Title</p>
-                    <p class="mb-5">Contact Info</p>
-                </a>
-            </li>
-            <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
-                <a class="">
-                    <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail.jpg" alt="..." class="w-75 w-md-100">
-                    <p class="mb-1 mt-3 h4">Employee Name</p>
-                    <p class="mb-1">Title</p>
-                    <p class="mb-5">Contact Info</p>
-                </a>
-            </li>
-            <!-- <div class="w-75 w-md-100"></div> FIX ME: Perhaps, this is unecessary. -->
-            <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
-                <a class="">
-                    <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail.jpg" alt="..." class="w-75 w-md-100">
-                    <p class="mb-1 mt-3 h4">Employee Name</p>
-                    <p class="mb-1">Title</p>
-                    <p class="mb-5">Contact Info</p>
-                </a>
-            </li>
-            <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
-                <a class="">
-                    <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail.jpg" alt="..." class="w-75 w-md-100">
-                    <p class="mb-1 mt-3 h4">Employee Name</p>
-                    <p class="mb-1">Title</p>
-                    <p class="mb-5">Contact Info</p>
-                </a>
-            </li>
-            <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
-                <a class="">
-                    <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail.jpg" alt="..." class="w-75 w-md-100">
-                    <p class="mb-1 mt-3 h4">Employee Name</p>
-                    <p class="mb-1">Title</p>
-                    <p class="mb-5">Contact Info</p>
-                </a>
-            </li>
-            <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
-                <a class="">
-                    <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail.jpg" alt="..." class="w-75 w-md-100">
-                    <p class="mb-1 mt-3 h4">Employee Name</p>
-                    <p class="mb-1">Title</p>
-                    <p class="mb-5">Contact Info</p>
-                </a>
-            </li>
-            <!-- <div class="w-75 w-md-100"></div> FIX ME: Perhaps, this is unecessary. -->
-        </ul>
+        <h2 class="mb-5 display-4">Staff</h2>
+        <section>
+            <h3 class="mb-5">Leadership</h3>
+            <ul class="list-unstyled row ucf-post-list-items flex-wrap" style="margin-bottom:-3rem;" >
+                <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
+                    <a class="">
+                        <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail-3.jpg" alt="..." class="w-75 w-md-100">
+                        <p class="mb-0 mt-3 font-weight-bold">Employee Name</p>
+                        <p class="mb-0">Title</p>
+                        <p class="mb-5">Contact Info</p>
+                    </a>
+                </li>
+                <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
+                    <a class="">
+                        <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail-3.jpg" alt="..." class="w-75 w-md-100">
+                        <p class="mb-0 mt-3 font-weight-bold">Employee Name</p>
+                        <p class="mb-0">Title</p>
+                        <p class="mb-5">Contact Info</p>
+                    </a>
+                </li>
+                <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
+                    <a class="">
+                        <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail-3.jpg" alt="..." class="w-75 w-md-100">
+                        <p class="mb-0 mt-3 font-weight-bold">Employee Name</p>
+                        <p class="mb-0">Title</p>
+                        <p class="mb-5">Contact Info</p>
+                    </a>
+                </li>
+            </ul>
+        </section>
+        <section class="mt-10">
+            <h3 class="mb-5" >Office Staff</h3>
+            <ul class="list-unstyled row ucf-post-list-items flex-wrap" style="margin-bottom:-3rem;" >
+                <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
+                    <a class="">
+                        <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail-3.jpg" alt="..." class="w-75 w-md-100">
+                        <p class="mb-0 mt-3 font-weight-bold">Employee Name</p>
+                        <p class="mb-0">Title</p>
+                        <p class="mb-5">Contact Info</p>
+                    </a>
+                </li>
+                <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
+                    <a class="">
+                        <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail-3.jpg" alt="..." class="w-75 w-md-100">
+                        <p class="mb-0 mt-3 font-weight-bold">Employee Name</p>
+                        <p class="mb-0">Title</p>
+                        <p class="mb-5">Contact Info</p>
+                    </a>
+                </li>
+            </ul>
+        </section>
+        <section class="mt-10">
+            <h3 class="mb-5" >Programming</h3>
+            <ul class="list-unstyled row ucf-post-list-items flex-wrap" style="margin-bottom:-3rem;" >
+                <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
+                    <a class="">
+                        <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail-3.jpg" alt="..." class="w-75 w-md-100">
+                        <p class="mb-0 mt-3 font-weight-bold">Employee Name</p>
+                        <p class="mb-0">Title</p>
+                        <p class="mb-5">Contact Info</p>
+                    </a>
+                </li>
+                <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
+                    <a class="">
+                        <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail-3.jpg" alt="..." class="w-75 w-md-100">
+                        <p class="mb-0 mt-3 font-weight-bold">Employee Name</p>
+                        <p class="mb-0">Title</p>
+                        <p class="mb-5">Contact Info</p>
+                    </a>
+                </li>
+                <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
+                    <a class="">
+                        <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail-3.jpg" alt="..." class="w-75 w-md-100">
+                        <p class="mb-0 mt-3 font-weight-bold">Employee Name</p>
+                        <p class="mb-0">Title</p>
+                        <p class="mb-5">Contact Info</p>
+                    </a>
+                </li>
+                <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
+                    <a class="">
+                        <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail-3.jpg" alt="..." class="w-75 w-md-100">
+                        <p class="mb-0 mt-3 font-weight-bold">Employee Name</p>
+                        <p class="mb-0">Title</p>
+                        <p class="mb-5">Contact Info</p>
+                    </a>
+                </li>
+                <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
+                    <a class="">
+                        <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail-3.jpg" alt="..." class="w-75 w-md-100">
+                        <p class="mb-0 mt-3 font-weight-bold">Employee Name</p>
+                        <p class="mb-0">Title</p>
+                        <p class="mb-5">Contact Info</p>
+                    </a>
+                </li>
+                <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
+                    <a class="">
+                        <img src="/wp-content/themes/ucfwordpresstheme/images/staff-thumbnail-3.jpg" alt="..." class="w-75 w-md-100">
+                        <p class="mb-0 mt-3 font-weight-bold">Employee Name</p>
+                        <p class="mb-0">Title</p>
+                        <p class="mb-5">Contact Info</p>
+                    </a>
+                </li>
+            </ul>
+        </section>
     </div>
 
 <!-- <?php get_template_part('includes/section', 'content'); ?> -->
